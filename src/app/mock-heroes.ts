7d40149f3473e964971ebcbd92900b4f952cec4f
Shasi Kanth Dulipalla <shasi.dulipalla@Yash.com>
2017-03-31 17:03:56 +0530

    First Commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Alice' },
+  { id: 2, name: 'Bobby' },
+  { id: 3, name: 'Cilic' },
+  { id: 4, name: 'Domino' },
+  { id: 5, name: 'Essar' }
+];
